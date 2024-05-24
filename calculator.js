e8cbb9576fa4cea461e3f64fb75d3322840be37a
Susanlt <susanlt@me.com>
2024-05-24 15:30:08 -0700

    added calculator.js

---
+++ calculator.js
@@ -0,0 +1,32 @@
+// Four variables with values that do not change
+const a = 150;
+const b = 5;
+const c = 2;
+const d = 25;
+
+// Math operations
+const addition = a + b;           
+const subtraction = d - c;        
+const multiplication = a * c;     
+const division = d / b;           
+const modulo = a % c;             
+const exponentiation = b ** c;    
+
+let increment = a;                
+increment++;                      
+
+let decrement = b;                
+decrement--;                      
+
+const negative = -c;              
+
+// console log results
+console.log("a + b:", addition);             
+console.log("d - c:", subtraction);          
+console.log("a * c:", multiplication);       
+console.log("d / b:", division);             
+console.log("a % c:", modulo);               
+console.log("b ** c:", exponentiation);      
+console.log("a++:", increment);              
+console.log("b--:", decrement);              
+console.log("-c:", negative);                
