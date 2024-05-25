706e60feb900d9e2a8f08a661ce679160b598b5c
Susanlt <susanlt@me.com>
2024-05-25 08:08:36 -0700

    added canDrive.js

---
+++ canDrive.js
@@ -0,0 +1,18 @@
+// Variables
+let firstName = "Lily";
+let lastName = "Greene";
+let age = 25; 
+let drivingAge = 16;
+
+// Comparing age with driving age
+if (age >= drivingAge) {
+    // Calculating driving years
+    let drivingYears = age - drivingAge;
+    console.log(`${firstName} ${lastName} has been driving for ${drivingYears} years.`);
+} else {
+    // Counting down from 5
+    for (let i = 5; i > 0; i--) {
+        console.log(i);
+    }
+    console.log("Too young to drive!");
+}
