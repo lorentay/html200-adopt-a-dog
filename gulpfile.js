f22bceeca8e0f8960d9e23e12d8ea6169b3fadca
Susanlt <susanlt@me.com>
2024-07-07 14:39:04 -0700

    Finished assignment content in index.html, gulpfile.jx, stysles.scss, _variables.scss,_mixin.scss. Also installed node, gulp, and all the related files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create(); 
 
+// Compile SCSS to CSS
 gulp.task('sass', function() {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream()); 
 });
 
+// Watch files for changes
 gulp.task('watch', function() {
   gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('./*.html').on('change', browserSync.reload); 
 });
 
-gulp.task('default', gulp.series('sass', 'watch'));
+// Initialize BrowserSync
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './' 
+    }
+  });
+});
+
+// Default task
+gulp.task('default', gulp.parallel('sass', 'watch', 'browser-sync'));
